1a36e75b2c3bafe1d32ff70840af2fc94309444f
A.Lepe <dev@alepe.com>
2020-04-19 17:44:59 +0900

    Created SCSS based in current CSS and updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,49 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	cssmin = require('gulp-cssmin'),
-	del = require('del');
+	sass   = require('gulp-sass'),
+	del    = require('del');
 
 var paths = {
 	prefix: 'frameplayer.min',
 	build: 'dist',
+	src  : 'src',
 	js: ['src/*.js'],
-	css: ['src/*.css']
+	css: ['src/*.css'],
+	scss: ['src/scss/*.scss']
 };
 
 gulp.task('clean', function(cb) {
   del(paths.build, cb);
 });
 
-gulp.task('js', function() {
+gulp.task('js', ["clean"], function() {
 	return gulp.src(paths.js)
 		.pipe(uglify())
 		.pipe(concat(paths.prefix + '.js'))
 		.pipe(gulp.dest(paths.build));
 });
 
-gulp.task('css', function() {
+gulp.task('css', ["clean"], function() {
     return gulp.src(paths.css)
 		.pipe(cssmin())
 		.pipe(concat(paths.prefix +  '.css'))
 		.pipe(gulp.dest(paths.build));
 });
 
+sass.compiler = require('node-sass');
+gulp.task('sass', ["clean", "css"], function () {
+	return gulp.src(paths.scss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(paths.src));
+});
+
 // Watch
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['js']);
   gulp.watch(paths.styles, ['css']);
+  gulp.watch(paths.scss, ['sass']);
 });
 
 // Build
-gulp.task('default', ['clean', 'js', 'css']);
+gulp.task('default', ['clean', 'js', 'sass', 'css']);
